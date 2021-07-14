ae9e2268f93554556951299d46a36dd11746981e
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-14 13:55:19 +0300

    İlk Hello World Uygulamasının Oluşturulması

---
+++ Calismalarim/console-ilk-hello-world-uygulamam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_ilk_hello_world_uygulamam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
