a9f0392b3a1c786bafcbcf7868f3cbe7dd044a88
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-15 11:24:54 +0300

    Karar yapıları dersi çalışma örnekleri yapıldı.

---
+++ Calismalarim/karar-yapilari-dersi-ornekleri/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace karar_yapilari_dersi_ornekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time=DateTime.Now.Hour;
+            if(time<=18)
+            Console.WriteLine("İyi Günler");
+            else
+            Console.WriteLine("İyi Günler");
+
+            string sonuc =time<=18? "iyi günler":"iyi geceler";
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
