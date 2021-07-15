d809a6a4883ce61e7e76d759fdc8c881374c5e8d
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-15 10:14:24 +0300

    Hata Kontrol dersi ile ilgili örnekler yapıldı.

---
+++ Calismalarim/hata-yonetimi-dersi-ornekleri/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace hata_yonetimi_dersi_ornekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hataya sebebiyet  verebilecek kod satırı burada yer almalıdır
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz");
+                int sayi=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi:"+sayi);
+            }
+            //hata sistem tarafından yakalanıp ekrana yazdırılabilir.
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata:" +ex.Message.ToString());                
+            }
+            // //tryda hata alsın veya almasın her zaman  çalışmasını istediğimiz kod parçacığı buraya gelecektir.
+            // finally
+            // {
+            //     Console.WriteLine("İşlem tamamlandı");
+            // }
+
+            //Eğer fırlatıcak hatanın nedenini tam olarak bilebiliyorsak catch içerisinde Arguman ex.FormatEx OverFlowEx kullanarak 
+            //kullanıcımıza hata mesajımızı özelleştirerek gösterebiliriz.
+           
+        }
+    }
+}
