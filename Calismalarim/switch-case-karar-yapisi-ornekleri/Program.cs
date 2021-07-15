71057c4da9819d064e2e7852cc3c1e6366fb72b4
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-15 21:13:25 +0300

    Switch-case karar yapıları örnekleri uygulandı.

---
+++ Calismalarim/switch-case-karar-yapisi-ornekleri/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace switch_case_karar_yapisi_ornekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month=DateTime.Now.Month;
+
+        //expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız");
+                    break;
+                case 7:
+                    Console.WriteLine("Temmuz Ayındasınız");
+                    break;
+
+                default:
+                Console.WriteLine("Yanlış bir değer girdiniz");
+                break;
+            }
+
+            switch (month)
+            {
+                case 1:
+                case 2:
+                case 4:
+                Console.WriteLine("Kış ayındasınız");
+                break;
+                
+                default:
+                break;
+            }
+        }
+    }
+}
