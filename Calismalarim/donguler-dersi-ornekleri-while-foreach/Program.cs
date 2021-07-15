e31ad38d08032332d537ce23b70b4e8efc2f76a5
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-15 22:24:31 +0300

    While, foreach ile ilgili örnekler yapıldı.

---
+++ Calismalarim/donguler-dersi-ornekleri-while-foreach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace donguler_dersi_ornekleri_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //While
+           //1 den başlarak girilen sayiya kadar(dahil) ortalama hesaplayıp yazdıran program
+           Console.WriteLine("Lütfen bir sayı giriniz");
+           int sayi=int.Parse(Console.ReadLine());
+           int sayac=1;
+           int toplam=0;
+           while(sayac<=sayi){
+               toplam+=sayac;
+               sayac++;
+
+           }
+           Console.WriteLine(toplam/sayi);
+
+            //A dan Z ye kadar tüm harfleri yazdır..
+            char character='a';
+            while (character<'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            //Foreach
+            string[] arabalar={"BMV","Ford","Toyota","Nissan"};
+            foreach(var araba in arabalar){
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
