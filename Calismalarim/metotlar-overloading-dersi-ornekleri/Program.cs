dc21a5bc2e9126e74c51200e6c40a6f0baf95a1a
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-16 20:58:20 +0300

    Metot aşırı yükleme ve altParametre ile ilgili ornekler yapildi.

---
+++ Calismalarim/metotlar-overloading-dersi-ornekleri/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace metotlar_overloading_dersi_ornekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //alt parametreler
+            string sayi="999";
+            int outSayi;
+            bool sonuc=int.TryParse(sayi,out outSayi);
+            if(sonuc){
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            Console.WriteLine("Başarısız");
+
+            Metotlar metotlar=new Metotlar();
+            metotlar.topla(3,2,out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot Aşırı Yükleme-Overloading
+            int ifade=99;
+            metotlar.ekranaYazdir(ifade);
+            
+        }
+    }
+    class Metotlar{
+        public void topla(int a, int b, out int toplam){
+            
+            toplam=a+b;
+        }
+        public void ekranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+        public void ekranaYazdir(int veri){
+            Console.WriteLine(veri);
+        }
+    }
+}
