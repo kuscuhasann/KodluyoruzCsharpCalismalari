1b3699ce69a636f4e9c4e333d03b82e1b0308644
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-16 20:45:21 +0300

    Metotlar ile iligli ornekler yapildi.

---
+++ Calismalarim/metodlar-dersi-ornekleri/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace metodlar_dersi_ornekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metotlar metotlar=new Metotlar();
+            int a=2,b=3;
+            int sonuc=metotlar.arttirveTopla(a,b);
+            Console.WriteLine(sonuc);
+
+           metotlar.ekranaYazdir(sonuc.ToString());
+        }
+        static int topla(int deger1,int deger2){
+            return deger1+deger2;
+        }
+    }
+    class Metotlar{
+        public void ekranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+
+        public int arttirveTopla(int deger1,int deger2)
+        {
+            deger1+=1;
+            deger2+=1;
+            return deger1+deger2;
+        }
+    }
+}
