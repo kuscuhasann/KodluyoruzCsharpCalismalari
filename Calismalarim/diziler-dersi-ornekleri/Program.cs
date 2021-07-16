09f70a0730a30a39a72aa15a230c798dcb36f162
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-16 10:10:48 +0300

    Diziler dersi ornekleri uygulandi.

---
+++ Calismalarim/diziler-dersi-ornekleri/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace diziler_dersi_ornekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string [] renkler=new string[5];
+            string[] hayvanlar={"Kedi","Köpek","Kuş","Maymun"};
+            int[] dizi;
+            dizi=new int[5];
+            //Dizilere Deger Atama ve Erisim
+            renkler[0]="Mavi";
+            dizi[3]=10;
+
+            Console.WriteLine(hayvanlar[1]);
+
+            //Döngüler dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz");
+            int diziUzunlugu=int.Parse(Console.ReadLine());
+            int[] sayiDizisi=new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz: ",i+1);
+                sayiDizisi[i]=int.Parse(Console.ReadLine());
+
+            }
+            int toplam=0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam+=sayi;
+            }
+            Console.WriteLine("Ortalama: "+toplam/diziUzunlugu);
+        }
+    }
+}
