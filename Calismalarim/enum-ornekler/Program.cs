96945e93a4b3237db60d17f29431ce54ac6d0f1f
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-26 21:40:55 +0300

    Enum çalışmaları

---
+++ Calismalarim/enum-ornekler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace enum_ornekler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Değeri belirli olan değişkenler olduğunda kodun okunabilirliği arttırmaktadır.
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik=32;
+            if(sicaklik<=(int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın ısınmasını bekle");
+            }
+            else if(sicaklik>=(int)HavaDurumu.Sıcak)
+            Console.WriteLine("Dışarıya çıkmak için sıcak bir gün");
+            else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<=(int)HavaDurumu.CokSıcak)
+            Console.WriteLine("Hadi dışarıya çıkalım");
+        }
+    }
+    enum Gunler
+    {
+        //Pazartesi-1 den başlatılırsa diğerleri +1 şeklinde olur
+        Pazartesi=1,
+        Sali,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar,
+    }
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=25,
+        Sıcak=25,
+        CokSıcak=30
+    }
+}
--- Calismalarim/struct-nedir/Program.cs
+++ Calismalarim/struct-nedir/Program.cs
@@ -6,6 +6,42 @@ namespace struct_nedir
     {
         static void Main(string[] args)
         {
-                    }
+            //Structlar değer tipindedir.
+
+            //Sınıf
+            Dikdortgen dikdortgen=new Dikdortgen();
+            dikdortgen.kisaKenar=3;
+            dikdortgen.uzunKenar=6;
+            Console.WriteLine("Class Alan Hesabı: {0}",dikdortgen.AlanHesapla());
+            //Struct
+            //Newlemeden çağrılabilir
+            //parametresiz constructora izin vermez.
+            //Neden Structları kullanmalıyız?
+            //*16bayt kadar struct 16bayttan sonra class kullanılmalı (Uzman önerisi..)
+            Dikdortgen_Struct dikdortgenS;
+            dikdortgenS.kisaKenar=3;
+            dikdortgenS.uzunKenar=6;
+            Console.WriteLine("Struct Alan Hesabı: {0}",dikdortgenS.AlanHesapla());
+        }
+    }
+    class Dikdortgen
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+
+        public long AlanHesapla(){
+            return kisaKenar*uzunKenar;
+        }
+
+    }
+    //Yukarıda yazılımış class'ın struct hali: 
+    struct Dikdortgen_Struct
+    {
+         public int kisaKenar;
+        public int uzunKenar;
+
+        public long AlanHesapla(){
+            return kisaKenar*uzunKenar;
+        }
     }
 }
